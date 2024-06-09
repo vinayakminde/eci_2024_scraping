03408a7fff8475fe0741b93102453f73012259e2
Vinayak Minde <vminde000@gmail.com>
2024-06-09 17:05:41 +0530

    Commit for sample change

--- eci_scrape.py
+++ eci_scrape.py
@@ -23,7 +23,7 @@ if response.status_code == 200:
             cols = row.find_all('td')
             # Get the text from each column and print it
             if cols:
-                cols = [col.text.strip() for col in cols]
+                cols = [col.text.strip() for col in cols] #smaple change
                 print(cols)
     else:
         print("Table not found")
